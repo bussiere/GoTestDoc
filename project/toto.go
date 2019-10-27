324adf8d97c9a14aa72bf7621f436228b0d77c92
bussiere <bussiere@gmail.com>
2019-10-27 15:10:16 +0100

    wip

--- project/toto.go
+++ project/toto.go
@@ -9,6 +9,15 @@ import (
 )
 
 // Hello to say hello
+func HelloBis(test string) string{
+	fmt.Println("hello world")
+	// we return string here
+	return test
+
+}
+
+
+// Hello to say hello
 func Hello() {
 	fmt.Println("hello world")
 
