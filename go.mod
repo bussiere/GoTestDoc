07a7c6a37168c6132ca0b0364400a85d9c1d5db7
bussiere <bussiere@gmail.com>
2019-10-27 10:21:31 +0100

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bussiere/GoTestDoc
+
+go 1.13
---
+++ toto.go
@@ -0,0 +1,20 @@
+// Copyright Some Company Corp.
+// All Rights Reserved
+package main
+
+
+// here are the imports
+import (
+	"fmt"
+)
+
+// function to say hello
+func hello() {
+	fmt.Println("hello world")
+
+}
+
+// Main function here
+func main() {
+	hello()
+}
